439e4f4010fee7db8fc0eaf671b2955d07cc0458
Sayuru De Alwis <saurudealwis@ymail.com>
2022-04-10 12:56:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import flask
+import datetime
+import wikipedia
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I %M %p')
+        print('Current time is ' + time)
+        talk('Current time is' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'who is alwis' in command:
+        talk('Sayuru de alwis is an Senior Web Developer and a upcoming flutter mobile application developer')
+    else:
+        talk('Please repeat it again')
+
+
+while True:
+    run_alexa()
